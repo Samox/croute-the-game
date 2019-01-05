eea39bf069b7ca2ec5d2405664d10bb4009bfedb
Sammyt <sammy.teillet@gmail.com>
2019-01-05 18:37:51 +0100

    feat(): Trigger command on a endpoint

--- src/application/movement/movement.app.ts
+++ src/application/movement/movement.app.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MovementController } from './movement.controller';
 import { MovementGateway } from './movement.gateway';
+import { CQRSModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [],
+  imports: [CQRSModule],
   controllers: [MovementController],
   providers: [MovementGateway],
 })
--- src/application/movement/movement.controller.ts
+++ src/application/movement/movement.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { MovementDto } from './interfaces/movement.dto';
 import { MovementGateway } from './movement.gateway';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateMovementCommand } from './commands/implementations/create-movement.command';
 
 @Controller('/movements')
 export class MovementController {
-  constructor(private readonly movementGateway: MovementGateway) {}
+  constructor(
+    private readonly movementGateway: MovementGateway,
+    private readonly commandBus: CommandBus,
+  ) {}
 
   @Post()
   getMovement(@Body() movementDto: MovementDto) {
     console.log(movementDto);
+    const createMovementCommand = new CreateMovementCommand(movementDto);
+    this.commandBus.execute(createMovementCommand);
   }
 
   @Get()
